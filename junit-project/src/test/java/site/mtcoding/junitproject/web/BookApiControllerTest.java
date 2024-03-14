6e9614cb5c23f95be642a369876d2af679bb03d7
hhmkorea <yuyee81@gmail.com>
2024-03-14 23:03:56 +0900

    pjt 엔티티 생성 완료.

--- junit-project/src/test/java/site/mtcoding/junitproject/web/BookApiControllerTest.java
+++ junit-project/src/test/java/site/mtcoding/junitproject/web/BookApiControllerTest.java
@@ -1,4 +1,12 @@
 package site.mtcoding.junitproject.web;
 
+import site.mtcoding.junitproject.domain.Book;
+
 public class BookApiControllerTest {
+    public void test() {
+        Book book = Book.builder()
+                .title("안녕")
+                .author("안녕")
+                .build();
+    }
 }
