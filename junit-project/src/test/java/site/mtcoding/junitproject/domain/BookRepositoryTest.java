9ac31f088c23ad39dab5269408afebc4578941f9
hhmkorea <yuyee81@gmail.com>
2024-03-15 19:02:02 +0900

    pjt select 테스트, given 사전 데이터 준비

--- junit-project/src/test/java/site/mtcoding/junitproject/domain/BookRepositoryTest.java
+++ junit-project/src/test/java/site/mtcoding/junitproject/domain/BookRepositoryTest.java
@@ -1,8 +1,13 @@
 package site.mtcoding.junitproject.domain;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
 
 @DataJpaTest // DB 와 관련된 컴포넌트만 메모리에 로딩
 public class BookRepositoryTest {
@@ -10,15 +15,66 @@ public class BookRepositoryTest {
     @Autowired // DI
     private BookRepository bookRepository;
 
+    //@BeforeAll // 테스트 시작 전에 한번만 실행
+    @BeforeEach // 각 테스트 시작 전에 한번씩 실행
+    public void prepareData(){
+        String title = "junit";
+        String author = "겟인데어";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+        bookRepository.save(book);
+    } // 트랜잭션 종료 됐다면 말이 안됨 :
+    // 가정 1 : [ prepareData() + 1 save ] (T종료) , [ prepareData() + 2 findAll ] (T종료) -> 사이즈 1 (검증 완료)
+    // 가정 2 : [ prepareData() + 1 save + prepareData() + 2 findAll ] (T종료) -> 사이즈 2 (검증 실패)
+
     // 1. 책 등록
     @Test
     public void save_test() {
-        System.out.println("save_test - 책등록 테스트 실행");
-    }
+        // given (데이터 준비)
+        String title = "junit5";
+        String author = "메타코딩";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+        // when (테스트 실행)
+        Book bookPS = bookRepository.save(book); // PS:Persisternce 영속화, DB에 저장된것
+
+        // then (검증)
+        Assertions.assertEquals(title, bookPS.getTitle());
+        Assertions.assertEquals(author, bookPS.getAuthor());
+    } // transaction 종료(저장된 데이터를 초기화함)
 
     // 2. 책 목록보기
+    @Test
+    public void findAll_test() {
+        // given (데이터 준비)
+        String title = "junit";
+        String author = "겟인데어";
+        // when (테스트 실행)
+        List<Book> bookPS = bookRepository.findAll();
+
+        System.out.println(" 사이즈: ===================================    : "+bookPS.size());
+        // then (검증)
+        Assertions.assertEquals(title, bookPS.get(0).getTitle());
+        Assertions.assertEquals(author, bookPS.get(0).getAuthor());
+    } // transaction 종료(저장된 데이터를 초기화함)
 
     // 3. 책 한건보기
+    @Test
+    public void findOne_test() {
+        // given (데이터 준비)
+        String title = "junit";
+        String author = "겟인데어";
+        // when (테스트 실행)
+        Book bookPS = bookRepository.findById(1L).get();
+
+        // then (검증)
+        Assertions.assertEquals(title, bookPS.getTitle());
+        Assertions.assertEquals(author, bookPS.getAuthor());
+    } // transaction 종료(저장된 데이터를 초기화함)
 
     // 4. 책 수정
 
