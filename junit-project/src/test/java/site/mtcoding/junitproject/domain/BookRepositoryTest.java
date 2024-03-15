1328b03cf4627e6f6a3d3a4c487da4bb76118e48
hhmkorea <yuyee81@gmail.com>
2024-03-15 20:30:43 +0900

    pjt delete 테스트??? 틀린값 넣었는데도 테스트 통과되는데?

--- junit-project/src/test/java/site/mtcoding/junitproject/domain/BookRepositoryTest.java
+++ junit-project/src/test/java/site/mtcoding/junitproject/domain/BookRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
 
@@ -24,7 +25,8 @@ public class BookRepositoryTest {
                 .title(title)
                 .author(author)
                 .build();
-        bookRepository.save(book);
+        Book bookPS = bookRepository.save(book);
+        System.out.println("------------------ prepareData : " + bookPS.getId());
     } // 트랜잭션 종료 됐다면 말이 안됨 :
     // 가정 1 : [ prepareData() + 1 save ] (T종료) , [ prepareData() + 2 findAll ] (T종료) -> 사이즈 1 (검증 완료)
     // 가정 2 : [ prepareData() + 1 save + prepareData() + 2 findAll ] (T종료) -> 사이즈 2 (검증 실패)
@@ -63,6 +65,7 @@ public class BookRepositoryTest {
     } // transaction 종료(저장된 데이터를 초기화함)
 
     // 3. 책 한건보기
+    @Sql("classpath:db/tableInit.sql")
     @Test
     public void findOne_test() {
         // given (데이터 준비)
@@ -76,7 +79,19 @@ public class BookRepositoryTest {
         Assertions.assertEquals(author, bookPS.getAuthor());
     } // transaction 종료(저장된 데이터를 초기화함)
 
-    // 4. 책 수정
+    // 4. 책 삭제
+    @Sql("classpath:db/tableInit.sql")
+    @Test
+    public void delete_test() {
+        // given
+        Long id = 2L;
+        System.out.println("------------------ delete_test : " + id);
+        // when
+        bookRepository.deleteById(id);
+        // then
+        Assertions.assertFalse(bookRepository.findById(id).isPresent()); // 값이 없으면 false
 
-    // 5. 책 삭제
+    }
+
+    // 5. 책 수정
 }
