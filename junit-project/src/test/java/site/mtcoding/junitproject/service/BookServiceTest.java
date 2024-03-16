1d4c7c9399a058167fb797f81b5872dc55660dd4
hhmkorea <yuyee81@gmail.com>
2024-03-16 20:54:36 +0900

    pjt JUnit테스트(Service) : Mock로 테스트

--- junit-project/src/test/java/site/mtcoding/junitproject/service/BookServiceTest.java
+++ junit-project/src/test/java/site/mtcoding/junitproject/service/BookServiceTest.java
@@ -1,20 +1,29 @@
 package site.mtcoding.junitproject.service;
 
-import org.junit.jupiter.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import site.mtcoding.junitproject.domain.BookRepository;
 import site.mtcoding.junitproject.util.MailSender;
-import site.mtcoding.junitproject.util.MailSenderStub;
 import site.mtcoding.junitproject.web.dto.BookRespDto;
 import site.mtcoding.junitproject.web.dto.BookSaveReqDto;
 
-@DataJpaTest
+@ExtendWith(MockitoExtension.class) // 가짜 메모리 환경
 public class BookServiceTest {
 
-    @Autowired // DI
-    private BookRepository bookRepository;
+    @InjectMocks
+    private BookService bookService;
+
+    @Mock
+    private BookRepository bookRepository; // 가짜로 bookReopsitory 만들기
+
+    @Mock
+    private MailSender mailSender;
 
     // 문제점 -> 서비스만 테스트하고 싶은데 Repository 레이어가 함께 테스트 된다는 점!!
     @Test
@@ -23,15 +32,18 @@ public class BookServiceTest {
         BookSaveReqDto dto = new BookSaveReqDto();
         dto.setTitle("JUnit강의");
         dto.setAuthor("메타코딩");
-        // stub
-        MailSenderStub mailSenderStub = new MailSenderStub();
+
+        // stub : 행동 정의, 가설
+        Mockito.when(bookRepository.save(ArgumentMatchers.any())).thenReturn(dto.toEntity());
+        Mockito.when(mailSender.send()).thenReturn(true);
 
         // when
-        BookService bookService = new BookService(bookRepository, mailSenderStub);
         BookRespDto bookRespDto = bookService.saveBook(dto);
 
         // then
-        Assertions.assertEquals(dto.getTitle(), bookRespDto.getTitle());
-        Assertions.assertEquals(dto.getAuthor(), bookRespDto.getAuthor());
+        //Assertions.assertEquals(dto.getTitle(), bookRespDto.getTitle());
+        //Assertions.assertEquals(dto.getAuthor(), bookRespDto.getAuthor());
+        assertThat(dto.getTitle()).isEqualTo(bookRespDto.getTitle());
+        assertThat(dto.getAuthor()).isEqualTo(bookRespDto.getAuthor());
     }
 }
