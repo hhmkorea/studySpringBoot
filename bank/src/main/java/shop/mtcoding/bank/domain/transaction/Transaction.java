5cb553ea2d428197cbd4bc857260cb3a6b7dc5be
hhmkorea <yuyee81@gmail.com>
2024-03-18 21:47:59 +0900

    bank, Entity 생성 : Account, Transaction

---
+++ bank/src/main/java/shop/mtcoding/bank/domain/transaction/Transaction.java
@@ -0,0 +1,69 @@
+package shop.mtcoding.bank.domain.transaction;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import shop.mtcoding.bank.domain.account.Account;
+import shop.mtcoding.bank.domain.user.User;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "transaction_tb")
+@Entity
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Account withdrawAccount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Account depositAccount;
+
+    private Long ammount;
+
+    private Long withdrawAccountBalance; // 1111 계좌 -> 1,000원 -> 500원 -> 200원
+    private Long depositAccountBalance;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TransactionEnum gubun; // WITHDRAW, DEPOSIT, TRANSFER, ALL
+
+    // 계좌가 사라져도 로그는 남아야 한다.
+    private String sender;
+    private String receiver;
+    private String tel;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public Transaction(Long id, Account withdrawAccount, Account depositAccount, Long ammount, Long withdrawAccountBalance, Long depositAccountBalance, TransactionEnum gubun, String sender, String receiver, String tel, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.withdrawAccount = withdrawAccount;
+        this.depositAccount = depositAccount;
+        this.ammount = ammount;
+        this.withdrawAccountBalance = withdrawAccountBalance;
+        this.depositAccountBalance = depositAccountBalance;
+        this.gubun = gubun;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.tel = tel;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
