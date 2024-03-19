f7fe5d14948871bf947a299bce15c92d929cc208
hhmkorea <yuyee81@gmail.com>
2024-03-19 23:18:57 +0900

    bank, 회원가입 : Service Test

--- bank/src/main/java/shop/mtcoding/bank/service/UserService.java
+++ bank/src/main/java/shop/mtcoding/bank/service/UserService.java
@@ -3,6 +3,7 @@ package shop.mtcoding.bank.service;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,37 +25,42 @@ public class UserService {
 
     // 서비스는 DTO를 요청받고, DTO로 응답한다.
     @Transactional // Transaction 메서드 시작할 때 시작되고, 종료될 때 함께 종료됨.
-    public JoinReqDto joinMember(JoinRequestDto joinRequestDto) {
+    public JoinRespDto joinMember(JoinReqDto joinReqDto) {
         // 1. 동일 유저네임 존재 검사
-        Optional<User> userOP = userRepository.findByUsername(joinRequestDto.getUsername());
+        Optional<User> userOP = userRepository.findByUsername(joinReqDto.getUsername()); // JPA에서 제공하는 쿼리
         if (userOP.isPresent()) {
             // 유저네임 중복되었다는 뜻
             throw new CustomApiException("동일한 username이 존재합니다.");
         }
         // 2. 패스워드 인코딩 + 회원가입
-        User userPS = userRepository.save(joinRequestDto.toEntity(passwordEncoder));
+        User userPS = userRepository.save(joinReqDto.toEntity(passwordEncoder)); // JPA에서 제공하는 쿼리
 
         // 3. dto 응답
-        return new JoinReqDto(userPS);
+        return new JoinRespDto(userPS);
     }
 
+    @ToString
     @Setter
     @Getter
-    public static class JoinReqDto {
+    public static class JoinRespDto {
         private Long id;
         private String username;
+        private String password;
+        private String email;
         private String fullname;
 
-        public JoinReqDto(User user) {
+        public JoinRespDto(User user) {
             this.id = user.getId();
             this.username = user.getUsername();
+            this.password = user.getPassword();
+            this.email = user.getEmail();
             this.fullname = user.getFullname();
         }
     }
 
     @Setter
     @Getter
-    public static class JoinRequestDto {
+    public static class JoinReqDto {
         // 유효성 검사
         private String username;
         private String password;
---
+++ bank/src/test/java/shop/mtcoding/bank/service/UserServiceTest.java
@@ -0,0 +1,64 @@
+package shop.mtcoding.bank.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import shop.mtcoding.bank.domain.user.User;
+import shop.mtcoding.bank.domain.user.UserEnum;
+import shop.mtcoding.bank.domain.user.UserRepository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import shop.mtcoding.bank.service.UserService.JoinReqDto;
+import shop.mtcoding.bank.service.UserService.JoinRespDto;
+
+// Spring 관련 Bean들이 하나도 없는 환경.
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    @InjectMocks
+    private UserService userService;
+    @Mock
+    private UserRepository userRepository;
+    @Spy // 진짜 psssword를 만들어서 @InjectMocks UserService에 넣음.
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Test
+    public void joinMember_test() throws Exception {
+        // given
+        JoinReqDto joinReqDto = new JoinReqDto();
+        joinReqDto.setUsername("ssar");
+        joinReqDto.setPassword("1234");
+        joinReqDto.setEmail("ssar@nate.com");
+        joinReqDto.setFullname("쌀");
+
+        // stub1 : 가정법, 가설
+        Mockito.when(userRepository.findByUsername(ArgumentMatchers.any())).thenReturn(Optional.empty());
+        //Mockito.when(userRepository.findByUsername(ArgumentMatchers.any())).thenReturn(Optional.of(new User()));
+
+        // stub2
+        User ssar = User.builder()
+                .id(1L)
+                .username("ssar")
+                .password("1234")
+                .email("ssar@nate.com")
+                .fullname("쌀")
+                .role(UserEnum.CUSTOMER)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+        Mockito.when(userRepository.save(ArgumentMatchers.any())).thenReturn(ssar);
+
+        // when
+        JoinRespDto joinRespDto = userService.joinMember(joinReqDto);
+        System.out.println("테스트: "+joinRespDto);
+
+        // then
+        Assertions.assertThat(joinRespDto.getId()).isEqualTo(1L);
+        Assertions.assertThat(joinRespDto.getUsername()).isEqualTo("ssar");
+    }
+}
