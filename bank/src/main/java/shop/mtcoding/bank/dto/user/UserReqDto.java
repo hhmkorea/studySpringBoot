769a280a0f4119f2b59dffca4600e5a7a14c35ea
hhmkorea <yuyee81@gmail.com>
2024-03-20 22:43:46 +0900

    bank : 유효성검사 - 정규표현식

--- bank/src/main/java/shop/mtcoding/bank/dto/user/UserReqDto.java
+++ bank/src/main/java/shop/mtcoding/bank/dto/user/UserReqDto.java
@@ -1,6 +1,7 @@
 package shop.mtcoding.bank.dto.user;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,13 +12,20 @@ public class UserReqDto {
     @Setter
     @Getter
     public static class JoinReqDto {
-
+        // 영문, 숫자는 되고, 길이는 최소 2~20자 내외
+        @Pattern(regexp = "", message = "영문/숫자 2~20자 이내로 작성해주세요.") // 정규표현식
         @NotEmpty // null이거나, 공백일 수 없다.
         private String username;
+
+        // 길이 4~20
         @NotEmpty
         private String password;
+
+        // 이메일 형식
         @NotEmpty
         private String email;
+
+        // 영어, 한글, 1~20
         @NotEmpty
         private String fullname;
 
---
+++ bank/src/test/java/shop/mtcoding/bank/temp/RegexTest.java
@@ -0,0 +1,37 @@
+package shop.mtcoding.bank.temp;
+
+import org.junit.jupiter.api.Test;
+
+public class RegexTest {
+    @Test
+    public void doKo_test() throws Exception {
+
+    }
+
+    @Test
+    public void dontKo_test() throws Exception {
+
+    }
+
+    @Test
+    public void doEn_test() throws Exception {
+
+    }
+
+    @Test
+    public void dontEn_test() throws Exception {
+
+    }
+
+    @Test
+    public void doEnNum_test() throws Exception {
+
+    }
+
+    @Test
+    public void doEn_ChkLen_test() throws Exception {
+
+    }
+
+    // username, email, fullname
+}
