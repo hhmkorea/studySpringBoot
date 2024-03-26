5ba00c93cd28b2008a9ee0918ab9ffa94bed956f
hhmkorea <yuyee81@gmail.com>
2024-03-26 23:09:19 +0900

    bank : unsuccessfulAuthentication 테스트

--- bank/src/test/java/shop/mtcoding/bank/config/jwt/JwtAuthenticationFilterTest.java
+++ bank/src/test/java/shop/mtcoding/bank/config/jwt/JwtAuthenticationFilterTest.java
@@ -1,7 +1,6 @@
 package shop.mtcoding.bank.config.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +12,16 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
 import shop.mtcoding.bank.config.dummy.DummyObject;
 import shop.mtcoding.bank.domain.user.UserRepository;
 import shop.mtcoding.bank.dto.user.UserReqDto.LoginReqDto;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 /**
  * packageName    : shop.mtcoding.bank.config.jwt
  * fileName       : JwtAuthenticationFilterTest
@@ -33,6 +35,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
  */
 // SpringBootTest 하는 곳에는 전부다 teardown.sql 을 붙여주자.
 @Sql("classpath:db/teardown.sql") // 실행시점 : BeforeEach 실행 직전마다!!
+@Transactional // 메서드 테스트 하면 자동 롤백
 @ActiveProfiles("test") // 쿼리 파라매터값까지 로그로 보여줌.
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
@@ -67,18 +70,29 @@ public class JwtAuthenticationFilterTest extends DummyObject {
         System.out.println("테스트 : " + jwtToken);
 
         // then
-        resultActions.andExpect(MockMvcResultMatchers.status().isOk());
-        Assertions.assertNotNull(jwtToken);
+        resultActions.andExpect(status().isOk());
+        assertNotNull(jwtToken);
         assertTrue(jwtToken.startsWith(JwtVO.TOKEN_PREFIX));
-        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.data.username").value("ssar"));
+        resultActions.andExpect(jsonPath("$.data.username").value("ssar"));
     }
 
     @Test
     public void unsuccessfulAuthentication_test() throws Exception {
         // given
+        LoginReqDto loginReqDto = new LoginReqDto();
+        loginReqDto.setUsername("ssar");
+        loginReqDto.setPassword("12345");
+        String reuquestBody = om.writeValueAsString(loginReqDto);
+        System.out.println("테스트 : " + reuquestBody);
 
         // when
+        ResultActions resultActions = mvc.perform(post("/api/login").content(reuquestBody).contentType(MediaType.APPLICATION_JSON));
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        String jwtToken = resultActions.andReturn().getResponse().getHeader(JwtVO.HEADER);
+        System.out.println("테스트 : " + responseBody);
+        System.out.println("테스트 : " + jwtToken);
 
         // then
+        resultActions.andExpect(status().isUnauthorized()); // 401
     }
 }
