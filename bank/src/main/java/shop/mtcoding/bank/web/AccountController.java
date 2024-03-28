028abfaf536a01b758442be9dc6c3539f44ac783
hhmkorea <yuyee81@gmail.com>
2024-03-28 23:19:15 +0900

    bank, 내계좌보기 : Controller 만들기

--- bank/src/main/java/shop/mtcoding/bank/web/AccountController.java
+++ bank/src/main/java/shop/mtcoding/bank/web/AccountController.java
@@ -6,14 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import shop.mtcoding.bank.config.auth.LoginUser;
 import shop.mtcoding.bank.dto.ResponseDto;
 import shop.mtcoding.bank.dto.account.AccountSaveReqDto;
 import shop.mtcoding.bank.dto.account.AccountSaveRespDto;
+import shop.mtcoding.bank.handler.ex.CustomForbiddenException;
 import shop.mtcoding.bank.service.AccountService;
 
 /**
@@ -41,5 +39,14 @@ public class AccountController {
         // AuthenticationPrincipal : 세션에 있는 값 처리
         AccountSaveRespDto accountSaveRespDto = accountService.saveAccount(accountSaveReqDto, loginUser.getUser().getId());
         return new ResponseEntity<>(new ResponseDto<>(1, "계좌등록 성공", accountSaveRespDto), HttpStatus.CREATED);
+    }
+
+    // 인증이 필요하고, account 테이블 데이타 다 주세요!!
+    // 인증이 필요하고, account 테이블에 login한 유저의 계좌만 주세요.
+    @GetMapping("/s/account/login-user")
+    public ResponseEntity<?> findUserAccount(@AuthenticationPrincipal LoginUser loginUser) {
+
+        AccountService.AccountListRespDto accountListRespDto = accountService.findListByUser(loginUser.getUser().getId());
+        return new ResponseEntity<>(new ResponseDto<>(1, "유저별 계좌 목록보기 성공", accountListRespDto), HttpStatus.OK);
     }
 }
