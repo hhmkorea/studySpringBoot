20ce60d16c43a12034a37bbf856b04c8b4d19e40
hhmkorea <yuyee81@gmail.com>
2024-03-29 22:31:00 +0900

    bank : JWT 만료기간 버그 처리

--- bank/src/test/java/shop/mtcoding/bank/config/jwt/JwtProcessTest.java
+++ bank/src/test/java/shop/mtcoding/bank/config/jwt/JwtProcessTest.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class JwtProcessTest {
 
-    private String createToken() {
+    private String createToken() {  // token 자동 생성 -- JWT 만료기간 버그 처리
         // given
         User user = User.builder().id(1L).role(UserEnum.CUSTOMER).build();
         LoginUser loginUser = new LoginUser(user); // id, role만 들어가 있음.
@@ -24,7 +24,7 @@ public class JwtProcessTest {
     public void create_test() throws Exception {
         // given
         // when
-        String jwtToken = createToken(); // token 자동 생성.
+        String jwtToken = createToken(); // token 자동 생성 -- JWT 만료기간 버그 처리
         System.out.println("테스트 : "+jwtToken);
 
         // then
@@ -34,7 +34,7 @@ public class JwtProcessTest {
     @Test
     public void verify_test() throws Exception {
         // given
-        String token = createToken(); // Bearer 제거해서 처리하기
+        String token = createToken(); // Bearer 제거해서 처리하기 -- JWT 만료기간 버그 처리
         String jwtToken = token.replace(JwtVO.TOKEN_PREFIX, "");
 
         // when
