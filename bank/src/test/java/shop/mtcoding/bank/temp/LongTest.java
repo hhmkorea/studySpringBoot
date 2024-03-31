b3dfeb607ce072ff1790c47e47e91de41eb454d4
hhmkorea <yuyee81@gmail.com>
2024-03-31 20:41:18 +0900

    bank : Long 타입 테스트

--- bank/src/test/java/shop/mtcoding/bank/temp/LongTest.java
+++ bank/src/test/java/shop/mtcoding/bank/temp/LongTest.java
@@ -1,6 +1,7 @@
 package shop.mtcoding.bank.temp;
 
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * packageName    : shop.mtcoding.bank.temp
@@ -15,7 +16,33 @@ import org.junit.jupiter.api.Test;
  */
 public class LongTest {
 
-@Test
+    @Test
+    public void long_test3() throws Exception {
+        // given
+        Long v1 = 128L;
+        Long v2 = 128L;
+
+        // when
+
+        // then
+        assertThat(v1).isEqualTo(v2);
+    }
+
+    @Test
+    public void long_test2() throws Exception {
+        // given (2의 8승 - 256 범위 (-126 + 127)까지는 비교가 됨), 그 이상일 경우 longValue를 붙여야함.
+        Long v1 = 128L;
+        Long v2 = 128L;
+        // when
+        if (v1  == v2) {
+            System.out.println("테스트 : v1이 동일합니다.");
+        } else {
+            System.out.println("테스트 : 동일하지 않습니다.");
+        }
+        // then
+    }
+
+    @Test
     public void long_test() throws Exception {
         // given
         Long number1 = 1111L;
