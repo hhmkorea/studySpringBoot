0b70a22d2fd9b156d7b2d8761ece195a50d43c66
hhmkorea <yuyee81@gmail.com>
2024-04-01 23:15:42 +0900

    bank, 입출금내역 : Service 만들기

---
+++ bank/src/main/java/shop/mtcoding/bank/dto/transaction/TransactionRespDto.java
@@ -0,0 +1,75 @@
+package shop.mtcoding.bank.dto.transaction;
+
+import lombok.Getter;
+import lombok.Setter;
+import shop.mtcoding.bank.domain.account.Account;
+import shop.mtcoding.bank.domain.transaction.Transaction;
+import shop.mtcoding.bank.util.CustomDateUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * packageName    : shop.mtcoding.bank.dto.transaction
+ * fileName       : TransactionRespDTo
+ * author         : dotdot
+ * date           : 2024-04-01
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-04-01        dotdot       최초 생성
+ */
+public class TransactionRespDto {
+
+    @Setter
+    @Getter
+    public static class TransactionListRespDto {
+        private List<TransactionDto> transactions = new ArrayList<>();
+
+        public TransactionListRespDto(List<Transaction> transactions, Account account) {
+            this.transactions = transactions.stream()
+                    .map((transaction) -> new TransactionDto(transaction, account.getNumber()))
+                    .collect(Collectors.toList());
+        }
+
+        @Setter
+        @Getter
+        public class TransactionDto {
+            private Long id;
+            private String gubun;
+            private String sender;
+            private String reciver;
+            private Long amount;
+            private Long tel;
+            private String createdAt;
+            private Long balance;
+
+            public TransactionDto(Transaction transaction, Long accountNumber) { // 1111 계좌
+                this.id = transaction.getId();
+                this.gubun = transaction.getGubun().getValue();
+                this.amount = transaction.getAmmount();
+                this.sender = transaction.getSender();
+                this.reciver = transaction.getReceiver();
+                this.createdAt = CustomDateUtil.toStringFormat(transaction.getCreatedAt());
+                this.tel = Long.valueOf(transaction.getTel() == null ? "없음" : transaction.getTel());
+
+                // 1111 계좌의 입출금 내역 (출금계좌 = null, 입금계좌  = 값) (입금계좌 = 값, 입금계좌  = null)
+                if (transaction.getDepositAccount() == null) { // 입금계좌가 null 이면
+                    this.balance = transaction.getWithdrawAccountBalance();
+                } else if (transaction.getWithdrawAccount() == null) {
+                    this.balance = transaction.getDepositAccountBalance();
+                } else {
+                    // 1111 계좌의 입출금 내역 (출금계좌 = 값, 입금계좌  = 값)
+                    if (accountNumber.longValue() == transaction.getDepositAccount().getNumber()) {
+                        this.balance = transaction.getDepositAccountBalance();
+                    } else {
+                        this.balance = transaction.getWithdrawAccountBalance();
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ bank/src/main/java/shop/mtcoding/bank/service/TransactionService.java
@@ -0,0 +1,44 @@
+package shop.mtcoding.bank.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import shop.mtcoding.bank.domain.account.Account;
+import shop.mtcoding.bank.domain.account.AccountRepository;
+import shop.mtcoding.bank.domain.transaction.Transaction;
+import shop.mtcoding.bank.domain.transaction.TransactionRepository;
+import shop.mtcoding.bank.handler.ex.CustomApiException;
+import shop.mtcoding.bank.dto.transaction.TransactionRespDto.TransactionListRespDto;
+
+import java.util.List;
+
+/**
+ * packageName    : shop.mtcoding.bank.service
+ * fileName       : TransactionService
+ * author         : dotdot
+ * date           : 2024-04-01
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-04-01        dotdot       최초 생성
+ */
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class TransactionService {
+    private final TransactionRepository transactionRepository;
+    private final AccountRepository accountRepository;
+
+    private TransactionListRespDto viewAccountList(Long userId, Long accountNumber, String gubun, int page) { // 입출금목록보기
+        Account accountPS = accountRepository.findByNumber(accountNumber)
+                .orElseThrow(() -> new CustomApiException("해당 계좌를 찾을 수 없습니다."));
+
+        accountPS.checkOwner(userId);
+
+        List<Transaction> transactionListPS = transactionRepository.findTransactionList(accountPS.getId(), gubun, page);
+
+        return new TransactionListRespDto(transactionListPS, accountPS);
+    }
+
+}
