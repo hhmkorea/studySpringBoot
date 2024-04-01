c5543f8ad74a12b5d7b56d3b31808535fd037a17
hhmkorea <yuyee81@gmail.com>
2024-04-01 18:47:29 +0900

    bank, 입출금내역 : 동적쿼리 작성

---
+++ bank/src/main/java/shop/mtcoding/bank/domain/transaction/TransactionRepositoryImpl.java
@@ -0,0 +1,73 @@
+package shop.mtcoding.bank.domain.transaction;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+/**
+ * packageName    : shop.mtcoding.bank.domain.transaction
+ * fileName       : TransactionRepositoryImpl
+ * author         : dotdot
+ * date           : 2024-04-01
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-04-01        dotdot       최초 생성
+ */
+
+interface Dao {
+    List<Transaction> findTransactionList(@Param("accountId") Long accountId,
+                                          @Param("gubun") String gubun,
+                                          @Param("page") Integer page);
+}
+
+// Impl은 꼭 붙여줘야 하고, TransactionRepository 가 앞에 붙어야 한다.
+@RequiredArgsConstructor
+public class TransactionRepositoryImpl implements Dao {
+    private final EntityManager em;
+
+    // JPQL 이 무엇인지
+    // join fetch
+    // outer 조인을 해야 하는지
+    @Override
+    public List<Transaction> findTransactionList(Long accountId, String gubun, Integer page) {
+        // 동적 쿼리(Dynamic Query, gubun 값을 가지고 동적쿼리 = DEPOSIT, WITHDRAW, ALL)
+        // JPQL
+        String sql = "";
+        sql += "select t from Transaction t ";
+
+        if (gubun.equals("WITHDRAW")) {
+            sql += "join fetch t.withdrawAccount wa ";
+            sql += "where t.withdrawAccount.id = :withdrawAccountId ";
+        } else if (gubun.equals("DEPOSIT")) {
+            sql += "join fetch t.depositAccount da ";
+            sql += "where t.depositAccount.id = :depositAccountId ";
+        } else { // gubun = ALL
+            sql += "left join fetch t.withdrawAccount wa ";
+            sql += "left join fetch t.depositAccount aa ";
+            sql += "where t.withdrawAccount.id = :withdrawAccountId ";
+            sql += "or ";
+            sql += "t.depositAccount.id = :depositAccountId ";
+        }
+
+        TypedQuery<Transaction> query = em.createQuery(sql, Transaction.class);
+
+        if (gubun.equals("WITHDRAW")) {
+            query = query.setParameter("withdrawAccountId", accountId);
+        } else if (gubun.equals("DEPOSIT")) {
+            query = query.setParameter("depositAccountId", accountId);
+        } else {
+            query = query.setParameter("withdrawAccountId", accountId);
+            query = query.setParameter("depositAccountId", accountId);
+        }
+
+        query.setFirstResult(page * 5); // 5, 10, 15
+        query.setMaxResults(5);
+
+        return query.getResultList();
+    }
+}
