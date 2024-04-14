5bd7c93a8d3140e8122508f97dcdcc465b5aecda
hhmkorea <yuyee81@gmail.com>
2024-04-14 23:34:44 +0900

    Blog : 소스 최신으로 변경함.

--- blog/src/main/java/com/cos/blog/config/SecurityConfig.java
+++ blog/src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -20,6 +20,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+// 최신 소스 : https://github.com/codingspecialist/Springboot-JPA-Blog/tree/update-202403
+
 // 1. 어노테이션 제거
 @Configuration
 public class SecurityConfig{ // 2. extends 제거
