849eebb65fad704994ba3206561a83cef8c6a574
hhmkorea <yuyee81@gmail.com>
2024-05-31 19:53:17 +0900

    Security : 환경셋팅

---
+++ security1/src/main/resources/db/query.sql
@@ -0,0 +1,15 @@
+-- 사용자 추가 : 유저이름@아이피주소
+create user 'cos'@'%' identified by 'cos1234';
+-- ON DB이름.테이블명
+-- 권한 설정 : TO 유저이름@아이피주소
+GRANT ALL PRIVILEGES ON *.* TO 'cos'@'%';
+-- 데이터베이스 생성
+CREATE DATABASE security CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+-- 데이터베이스 사용
+use security;
+-- 사용자 조회
+select * from mysql.user;
+
+-- 한글설정 확인
+show variables like 'c%';
+
