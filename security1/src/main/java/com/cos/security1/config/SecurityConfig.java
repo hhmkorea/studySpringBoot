6f91b4ad8029ed1b513bf1ed2cb968565f84d90d
hhmkorea <yuyee81@gmail.com>
2024-06-01 20:41:52 +0900

    Security1 : SecurityConfig

---
+++ security1/src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.cos.security1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * packageName    : com.cos.security1.config
+ * fileName       : SecurityConfig
+ * author         : dotdot
+ * date           : 2024-06-01
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-06-01        dotdot       최초 생성
+ */
+
+@Configuration
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true) // 특정 주소 접근시 권한 및 인증을 위한 어노테이션 활성화.
+public class SecurityConfig {
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encodePwd() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // 최신 버전(2.7)으로 시큐리티 필터 변경
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 1. csrf 비활성화
+        http.csrf(c -> c.disable());
+
+        // 2. 인증 주소 설정 (WEB-INF/** 추가해줘야 함. 아니면 인증이 필요한 주소로 무한 리다이렉션 일어남)
+        http.authorizeHttpRequests(a -> a
+                .requestMatchers("/user/**").authenticated()
+                //.requestMatchers("/admin/**").hasRole("ROLE_ADMIN")
+                .anyRequest().permitAll()
+        );
+
+        // 3. 로그인 처리 프로세스 설정
+        http.formLogin(f -> f.loginPage("/login")
+                .loginProcessingUrl("/loginProc")
+                .defaultSuccessUrl("/")
+        );
+
+        return http.build();
+    }
+}
--- security1/src/main/java/com/cos/security1/controller/IndexController.java
+++ security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.cos.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * packageName    : com.cos.security1.controller
@@ -24,5 +25,36 @@ public class IndexController {
         // mustache 기본폴더 src/main/resources/
         // ViewResolver 설정 : templates (prefix), .mustache(suffix) pom.xml로 의존성 삽입하면 생략가능.
         return "index"; // src/main/resources/templates/index.mustache
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user() { // @ResponseBody : text와 json 데이터 뿌려줌.
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager() {
+        return "manager";
+    }
+
+    // 스프링 시큐리티가 해당 주소 낚아채감.
+    @GetMapping("/login")
+    public @ResponseBody String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입 완료됨!!";
     }
 }
