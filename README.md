32dd985f615123f13d645aedd20f5d310189d18b
hhmkorea <yuyee81@gmail.com>
2024-08-08 20:57:01 +0900

    Security1 : test

--- README.md
+++ README.md
@@ -17,3 +17,7 @@
 
 > 최신 소스 : https://github.com/codingspecialist/Springboot-JPA-Blog/tree/update-202403
 + blog
+####
+#### 4. 메타코딩 '스프링부트 시큐리티 & JWT' 강의 실습
+> 강의 URL : https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81%EB%B6%80%ED%8A%B8-%EC%8B%9C%ED%81%90%EB%A6%AC%ED%8B%B0/dashboard
++ security1
---
+++ security1/target/classes/db/query.sql
@@ -0,0 +1,24 @@
+-- 사용자 추가 : 유저이름@아이피주소
+create user 'cos'@'%' identified by 'cos1234';
+-- ON DB이름.테이블명
+-- 권한 설정 : TO 유저이름@아이피주소
+GRANT ALL PRIVILEGES ON *.* TO 'cos'@'%';
+-- 데이터베이스 생성
+CREATE DATABASE security CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+-- 데이터베이스 사용
+use security;
+-- 사용자 조회
+select * from mysql.user;
+
+-- 한글설정 확인
+show variables like 'c%';
+
+desc user;
+
+select * from user;
+
+-- 권한변경 : ROLE_ 빼야함!!!
+update user set role = 'USER' where id = 1;
+update user set role = 'MANAGER' where id = 2;
+update user set role = 'ADMIN' where id = 3;
+COMMIT;
