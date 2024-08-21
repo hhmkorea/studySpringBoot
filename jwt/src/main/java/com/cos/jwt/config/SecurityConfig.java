1505380a6f31b0b073cb4945a86f9512d77c6f9f
hhmkorea <yuyee81@gmail.com>
2024-08-21 23:07:32 +0900

    jwt : model 추가, USER

---
+++ jwt/src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.cos.jwt.config;
+
+/**
+ * packageName    : com.cos.jwt.config
+ * fileName       : SecurityConfig
+ * author         : dotdot
+ * date           : 2024-08-21
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-08-21        dotdot       최초 생성
+ */
+public class SecurityConfig {
+}
---
+++ jwt/src/main/java/com/cos/jwt/model/User.java
@@ -0,0 +1,44 @@
+package com.cos.jwt.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * packageName    : com.cos.jwt.model
+ * fileName       : User
+ * author         : dotdot
+ * date           : 2024-08-21
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-08-21        dotdot       최초 생성
+ */
+
+@Data // Getter, Setter
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // MYSQL이면 auto increment
+    private long id;
+    private String username;
+    private String password;
+    private String roles; // USER, ADMIN
+
+    public List<String> getRoleList() { // size 2개, 0:USER, 1:ADMIN
+        if(this.roles.length() > 0) {
+            return Arrays.asList(this.roles.split(","));
+        }
+        return new ArrayList<>();
+    }
+
+
+}
