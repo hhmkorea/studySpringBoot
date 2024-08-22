04d2917de8a4075ac33954c20446c1e34886b866
hhmkorea <yuyee81@gmail.com>
2024-08-22 22:46:22 +0900

    jwt : JWT 임시 토큰 만들어 test

--- jwt/src/main/java/com/cos/jwt/filter/MyFilter1.java
+++ jwt/src/main/java/com/cos/jwt/filter/MyFilter1.java
@@ -1,6 +1,8 @@
 package com.cos.jwt.filter;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
@@ -19,7 +21,18 @@ public class MyFilter1 implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        // 토큰 : 코스
+        if(req.getMethod().equals("POST")) {
+            System.out.println("POST 요청됨");
+            String headerAuth = req.getHeader("Authorization");
+            System.out.println(headerAuth);
+        }
+
         System.out.println("필터1");
-        chain.doFilter(request, response); // 계속 프로세스 진행을 위해 chain에 넘겨줘야함.
+        chain.doFilter(req, res); // 계속 프로세스 진행을 위해 chain에 넘겨줘야함.
     }
 }
