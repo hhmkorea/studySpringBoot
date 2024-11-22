94df24c4b18cea635228f40868674bcb03ea7c1a
hhmkorea <hhmkorea@nate.com>
2024-11-22 22:04:46 +0900

    reflect : method에 annotation 적용.

--- reflect/src/com/cos/reflect/controller/UserController.java
+++ reflect/src/com/cos/reflect/controller/UserController.java
@@ -1,20 +1,30 @@
 package com.cos.reflect.controller;
 
+import com.cos.reflect.anno.RequestMapping;
+
 public class UserController {
 	
-	public void join() {
+	@RequestMapping("/join")
+	public String join() {
 		System.out.println("join() 함수 호출됨");
-	}
-	
-	public void login() {
-		System.out.println("login() 함수 호출됨");		
-	}
-	
-	public void user() {
-		System.out.println("user() 함수 호출됨");		
+		return "/";
 	}
 
-	public void hello() {
+	@RequestMapping("/login")
+	public String login() {
+		System.out.println("login() 함수 호출됨");		
+		return "/";
+	}
+
+	@RequestMapping("/user")
+	public String user() {
+		System.out.println("user() 함수 호출됨");		
+		return "/";
+	}
+
+	@RequestMapping("/hello")
+	public String hello() {
 		System.out.println("hello() 함수 호출됨");		
+		return "/";
 	}
 }
--- reflect/src/com/cos/reflect/filter/Dispatcher.java
+++ reflect/src/com/cos/reflect/filter/Dispatcher.java
@@ -1,17 +1,20 @@
 package com.cos.reflect.filter;
 
 import java.io.IOException;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.cos.reflect.anno.RequestMapping;
 import com.cos.reflect.controller.UserController;
 
 // 분기 시키기 - 라우터 역할
@@ -33,6 +36,7 @@ public class Dispatcher implements Filter {
 		System.out.println("엔드포인트 : " + endPoint);
 		
 		UserController userController = new UserController();
+// 1. 리플렉션 적용 전 ----------------
 //		if (endPoint.equals("/join")) {
 //			userController.join();
 //		} else if (endPoint.equals("/login")) {
@@ -43,14 +47,33 @@ public class Dispatcher implements Filter {
 		
 		// 리플렉션 -> 메서드를 런타임 시점에서 찾아내서 실행, 
 		Method[] methods = userController.getClass().getDeclaredMethods(); // 그 파일의 메서드만!! 
-		for (Method method : methods) {
-			//System.out.println(method.getName());
-			if (endPoint.equals("/" + method.getName())) {
+// 2. 리플렉션 적용 후 ----------------
+//		for (Method method : methods) {
+//			//System.out.println(method.getName());
+//			if (endPoint.equals("/" + method.getName())) {
+//				try {
+//					method.invoke(userController);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		}
+		
+		for (Method method:methods) { // 4바퀴 (join, login, user, hello)
+			Annotation annotation = method.getDeclaredAnnotation(RequestMapping.class); 
+			RequestMapping requestMapping = (RequestMapping) annotation; // 다운캐스팅한 이유? 
+			System.out.println(requestMapping.value()); // 주소를 annotation에 걸어서 함수 호출 가능.
+			
+			if (requestMapping.value().equals(endPoint)) {
 				try {
-					method.invoke(userController);
+					String path = (String)method.invoke(userController);
+					
+					RequestDispatcher dis = request.getRequestDispatcher(path); // 내부에서 실행하기 때문에 필터를 안탐. 외부에서는 필터 들어와서 못탐.
+					dis.forward(request, response);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
+				break;
 			}
 		}
 	}
