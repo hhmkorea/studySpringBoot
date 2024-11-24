930c10a7f0f756280e33ade86ba6d9458ffd23df
hhmkorea <hhmkorea@nate.com>
2024-11-24 20:10:40 +0900

    .

--- fluxtest/src/main/java/com/cos/fluxtest/MyFilter.java
+++ fluxtest/src/main/java/com/cos/fluxtest/MyFilter.java
@@ -18,17 +18,17 @@ public class MyFilter implements Filter {
 		System.out.println("필터 실행됨");
 		
 		HttpServletResponse servletResponse = (HttpServletResponse) response;
-		servletResponse.setContentType("text/plan; charset=utf-8");
+		servletResponse.setContentType("text/plain; charset=utf-8");
 		PrintWriter out = servletResponse.getWriter();
 		for (int i = 0; i < 5; i++) {
-			out.println("응답 : " + i);
+			out.println("응답 : " + i); // 버퍼에 데이타 쌓기.
+			out.flush(); // 버퍼를 비우다. 
 			try {
 				Thread.sleep(1000);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-		out.flush();
 	}
 
 }
