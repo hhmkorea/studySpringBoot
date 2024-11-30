e488662604eb4c050e46ac4196996c86b843912d
hhmkorea <hhmkorea@nate.com>
2024-11-30 18:34:39 +0900

    chatapp : Tailable, SSE protocol로 채팅 구현.

---
+++ chatapp/src/main/java/com/cos/chatapp/ChatController.java
@@ -0,0 +1,34 @@
+package com.cos.chatapp;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+@RequiredArgsConstructor // 생성자 만들어줌. 
+@RestController // 데이터 리턴하는 서버
+public class ChatController {
+	
+	private final ChatRepository chatRepository;
+	
+	@GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE) // SSE 프로토콜, response 계속 유지.
+	public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver) { // 경로에서 넘어온 파라매터 받음.
+		return chatRepository.mFindBySender(sender, receiver)
+				.subscribeOn(Schedulers.boundedElastic());
+	}
+	
+	@PostMapping("/chat")
+	public Mono<Chat> setMsg(@RequestBody Chat chat) { // 한 건만 데이터 리턴 
+		chat.setCreatedAt(LocalDateTime.now());
+		return chatRepository.save(chat);
+	}
+}
