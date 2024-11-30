7e972db09a83ad9b6677c317950eeea06e39e1f5
hhmkorea <hhmkorea@nate.com>
2024-11-30 22:22:26 +0900

    chat-front : 서버(chatapp) 연결 EventSource 객에 사용 chatapp : JS 접근 허용, @CrossOrigin

--- chat-front/js/chat.js
+++ chat-front/js/chat.js
@@ -1,4 +1,12 @@
-function getMsg(msg) {
+const eventSouce = new EventSource("http://localhost:8080/sender/ssar/receiver/cos");
+
+eventSouce.onmessage = (event) => {
+    console.log(1, event);
+    const data = JSON.parse(event.data);
+    console.log(2, data);
+}
+
+function getSendMsgBox(msg) {
     return `<div className="sent_msg">
     <p>${msg}</p>
     <span className="time_date"> 11:18 | Today</span>
@@ -14,7 +22,7 @@ document.querySelector("#chat-send").addEventListener("click", () => {
 
     let chatOutgoingBox = document.createElement("div");
     chatOutgoingBox.className = "outgoing_msg";
-    chatOutgoingBox.innerHTML = getMsg(msgInput.value);
+    chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value);
     chatBox.append(chatOutgoingBox);
     msgInput.value = "";
     //chatOutgoingBox.innerHTML = "안녕";
@@ -32,7 +40,7 @@ document.querySelector("#chat-outgoing-msg").addEventListener("keydown", (e) => 
 
         let chatOutgoingBox = document.createElement("div");
         chatOutgoingBox.className = "outgoing_msg";
-        chatOutgoingBox.innerHTML = getMsg(msgInput.value);
+        chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value);
         chatBox.append(chatOutgoingBox);
         msgInput.value = "";
         //chatOutgoingBox.innerHTML = "안녕";
--- chatapp/src/main/java/com/cos/chatapp/ChatController.java
+++ chatapp/src/main/java/com/cos/chatapp/ChatController.java
@@ -3,6 +3,7 @@ package com.cos.chatapp;
 import java.time.LocalDateTime;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,13 +20,15 @@ import reactor.core.scheduler.Schedulers;
 public class ChatController {
 	
 	private final ChatRepository chatRepository;
-	
+
+	@CrossOrigin // JS 요청을 허용 
 	@GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE) // SSE 프로토콜, response 계속 유지.
 	public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver) { // 경로에서 넘어온 파라매터 받음.
 		return chatRepository.mFindBySender(sender, receiver)
 				.subscribeOn(Schedulers.boundedElastic());
 	}
-	
+
+	@CrossOrigin // JS 요청을 허용 
 	@PostMapping("/chat")
 	public Mono<Chat> setMsg(@RequestBody Chat chat) { // 한 건만 데이터 리턴 
 		chat.setCreatedAt(LocalDateTime.now());
